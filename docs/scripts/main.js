7db51376969ad6c61fba4515f651a378de9eec55
nutritsio <serhii.web.dev@gmail.com>
2019-04-14 15:37:56 +0300

    Connect to Github Pages

---
+++ docs/scripts/main.js
@@ -0,0 +1,15 @@
+
+$('.phones .dropdown').click(function(){
+    $('.phones .dropdown .dropdown-menu')
+      .toggleClass('show');
+  });
+  
+  $('.phones .dropdown .dropdown-menu .dropdown-item').click(function () {
+  
+    $('.phones .dropdown .dropdown-toggle').text(this.innerHTML);
+  });
+  
+  // lightbox.option({
+  //   'resizeDuration': 200,
+  //   'wrapAround': true
+  // });
---
+++ docs/scripts/marquiz.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    Marquiz.init({ 
+      id: '5c02f41b4de4050055a57756', 
+      autoOpen: false, 
+      autoOpenFreq: 'once', 
+      openOnExit: false 
+    });
+  });
